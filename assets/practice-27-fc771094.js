b56647753665941255429344ce129241d088a425
MyroslavShendrik <citrus.user2204@gmail.com>
2025-09-21 05:35:01 +0000

    Deploying to gh-pages from @ MyroslavShendrik/js-homework-vite@7ae7631f55ff1f9de2075b081f3b8143c286dee8 🚀

--- assets/practice-27-fc771094.js
+++
@@ -1,7 +0,0 @@
-import"./modulepreload-polyfill-3cfb730f.js";//! Метод JSON.stringify()
-console.log(`%c
-    JSON.stringify(object)
-    `,"color: blue; font-size: 18px");console.log("--------------------------------------------------");const o={name:"Mango",age:3,isHappy:!0};console.log("dog1:",o);const n=JSON.stringify(o);console.log("dogJSON1:",n);//! "{"name":"Mango","age":3,"isHappy":true}"
-console.log("--------------------------------------------------");const l={name:"Mango",age:3,isHappy:!0,bark(){console.log("Woof!")}},s=JSON.stringify(l);console.log("dogJSON2:",s);//! "{"name":"Mango","age":3,"isHappy":true}"
-console.log("--------------------------------------------------");const g=JSON.stringify(()=>console.log("Well, this is awkward"));console.log("functionJSON:",g);//! undefined
-console.log("--------------------------------------------------");
