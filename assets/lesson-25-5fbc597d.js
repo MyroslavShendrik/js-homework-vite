5ca7ba6f29863c861de1b90a7ad3d5a086d2dc58
MyroslavShendrik <citrus.user2204@gmail.com>
2025-11-21 18:48:05 +0000

    Deploying to gh-pages from @ MyroslavShendrik/js-homework-vite@02052333f2fc3cba2306ae26cf07c8c48a306ba0 🚀

--- assets/lesson-25-5fbc597d.js
+++ assets/lesson-25-5fbc597d.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill-3cfb730f.js";import{H as a}from"./handlebars-aa0ac26f.js";const n=[{id:1,name:"Laptop",price:1500,description:"A high-performance laptop for all your needs."},{id:2,name:"Smartphone",price:700,description:"A modern smartphone with an excellent camera."},{id:3,name:"Headphones",price:200,description:"Noise-cancelling headphones for better focus."}],c=`<div class="card">
+import"./modulepreload-polyfill-3cfb730f.js";import{H as a}from"./handlebars-7f89a36c.js";const n=[{id:1,name:"Laptop",price:1500,description:"A high-performance laptop for all your needs."},{id:2,name:"Smartphone",price:700,description:"A modern smartphone with an excellent camera."},{id:3,name:"Headphones",price:200,description:"Noise-cancelling headphones for better focus."}],c=`<div class="card">
     <p><b>id:</b>{{id}}</p>
     <p><b>name:</b>{{name}}</p>
     <p><b>price:</b>{{price}}</p>
