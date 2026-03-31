4bfb9680b36a7ca2935649aee6146ce5d20b6a7e
MyroslavShendrik <citrus.user2204@gmail.com>
2026-03-31 13:56:58 +0300

    Update homework-33.js

--- src/lesson-33/js/homework-33.js
+++ src/lesson-33/js/homework-33.js
@@ -1,21 +1,47 @@
 const postsContainer = document.getElementById('posts');
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(posts => {
-    const markup = posts
-      .map(post => {
-        return `
-          <div class="post-card">
-            <h2>${post.title}</h2>
-            <p>${post.body}</p>
-          </div>
-        `;
-      })
-      .join('');
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => response.json())
+//   .then(posts => {
+//     const markup = posts
+//       .map(post => {
+//         return `
+//           <div class="post-card">
+//             <h2>${post.title}</h2>
+//             <p>${post.body}</p>
+//           </div>
+//         `;
+//       })
+//       .join('');
+
+//     postsContainer.innerHTML = markup;
+//   })
+//   .catch(error => {
+//     console.log('Помилка:', error);
+//   });
+
+function getAllPosts(){
+fetchAllPosts()
+.then(posts => renderPosts(posts))
+.catch(error => console.log(error));
+};
+function renderPosts(posts){
+   const markup = posts
+       .map(post => {
+         return `
+           <div class="post-card">
+             <h2>${post.title}</h2>
+             <p>${post.body}</p>
+           </div>
+         `;
+       })
+       .join('');
 
     postsContainer.innerHTML = markup;
-  })
-  .catch(error => {
-    console.log('Помилка:', error);
-  });
+};
+function fetchAllPosts(){
+ return fetch('https://jsonplaceholder.typicode.com/posts')
+   .then(response => response.json());
+
+};
+getAllPosts();
