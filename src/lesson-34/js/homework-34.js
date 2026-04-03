52cc298ad86f71540d82a2caf6ff57f4a68b8929
MyroslavShendrik <citrus.user2204@gmail.com>
2026-04-03 13:35:16 +0300

    lesson

--- src/lesson-34/js/homework-34.js
+++ src/lesson-34/js/homework-34.js
@@ -2,35 +2,5 @@ const input = document.getElementById('count');
 const button = document.getElementById('get-data');
 const dataList = document.getElementById('data-list');
 
-const BASE_URL = 'https://newsapi.org/v2/everything';
+const BaseURL = 'https://newsapi.org/v2/everything?q=apple&from=2026-04-02&to=2026-04-02&sortBy=popularity&apiKey=639ac27a9d1f4a569d7448e2fa14c42d';
 
-button.addEventListener('click', () => {
-  const count = input.value;
-
-  const searchParams = new URLSearchParams({
-    q: 'apple',
-    sortBy: 'popularity',
-    pageSize: count,
-    apiKey: '639ac27a9d1f4a569d7448e2fa14c42d',
-  });
-
-  fetch(`${BASE_URL}?${searchParams}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Помилка HTTP: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      dataList.innerHTML = '';
-
-      data.articles.forEach(article => {
-        const li = document.createElement('li');
-        li.textContent = article.title;
-        dataList.appendChild(li);
-      });
-    })
-    .catch(error => {
-      console.log('Помилка:', error);
-    });
-});
