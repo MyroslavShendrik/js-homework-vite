71f6e30c1a7b78a7c1105e4a2bea4cfb61adb7b9
MyroslavShendrik <citrus.user2204@gmail.com>
2026-04-03 14:50:16 +0300

    Update homework-34.js

--- src/lesson-34/js/homework-34.js
+++ src/lesson-34/js/homework-34.js
@@ -1,13 +1,17 @@
-const input = document.getElementById('count');
+const inputNews = document.getElementById('count');
+console.log("inputNews:",inputNews);
 const button = document.getElementById('get-data');
 const dataList = document.getElementById('data-list');
+
 
 const BaseURL = 'https://newsapi.org';
 const EndPoints = '/v2/everything';
 // const ApiKEY = '639ac27a9d1f4a569d7448e2fa14c42d';
+const nNews = parseInt(inputNews.value);
+console.log("nNews:",nNews);
 const params = {
    q: 'bitcoin',
-   pageSize:Number(input.value), 
+   pageSize: nNews, 
    apiKey: '639ac27a9d1f4a569d7448e2fa14c42d'
 };
 // const url = `${BaseURL}${EndPoints}?q=${params}&apiKey=${ApiKEY}`;
