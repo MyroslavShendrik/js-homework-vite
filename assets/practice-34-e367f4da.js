69137108345d58d4b03a7a52824e3dc8e109a88e
MyroslavShendrik <citrus.user2204@gmail.com>
2026-04-14 13:00:26 +0000

    Deploying to gh-pages from @ MyroslavShendrik/js-homework-vite@4d7e0392a054830082587508262123e8db9ca0c6 🚀

---
+++ assets/practice-34-e367f4da.js
@@ -0,0 +1,5 @@
+import"./modulepreload-polyfill-3cfb730f.js";//! Пагінація
+console.warn(`Вся колекція: 
+ ${window.location.href.split("/").slice(0,-2).join("/")+"/"}lesson-FE4_17/images/no-pagination.png`);console.log("--------------------------------------------------------------");//! пошук довжини колекції 
+const s=[5,7,9,15,16],e=[25,27,29,215,216,156,220],t=[35,37,39];function n(o){console.log("arr:",o);const l=o.length;return console.log("finalArray:",l),o.length}console.log(n(s));console.log(n(e));console.log(n(t));//!GET /posts?_page=1&_limit=25 - приклад запиту з урахуванням номера сторінки та кількості елементів на сторінці 
+я;
