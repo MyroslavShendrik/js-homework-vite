824af9ea7bd303712cf82601dfd70ac184b492f5
MyroslavShendrik <citrus.user2204@gmail.com>
2026-05-05 10:47:10 +0000

    Deploying to gh-pages from @ MyroslavShendrik/js-homework-vite@45282866576ccac3dc1bd126501ec34cc14a3e2a 🚀

---
+++ assets/practice-36-7f7d35b8.js
@@ -0,0 +1,2 @@
+import"./modulepreload-polyfill-3cfb730f.js";const t="http://localhost:3000/",s="users",n=`${t}/${s}`;//! ================= READ (GET всі) =================
+fetch(n).then(o=>o.json()).then(o=>console.log("GET all users:",o)).catch(o=>console.log(o));//! ================= READ (GET один) =================
