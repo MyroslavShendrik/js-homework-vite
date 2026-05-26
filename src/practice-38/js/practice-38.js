2fa616019911d46ac6b81d1f7bc40b4a09ee2840
MyroslavShendrik <citrus.user2204@gmail.com>
2026-05-26 13:59:14 +0300

    test push

---
+++ src/practice-38/js/practice-38.js
@@ -0,0 +1,8 @@
+console.log(
+    '%c 5.Приклади CRUD HTTP-запитів з async/await   \n   і try...catch та використанням json-server ',
+    'color: white; background-color: #D33F49',
+);
+
+//! Приклади CRUD HTTP-запитів з async/await та використанням json-server
+console.warn("Для демонстрації прикладів необхідно: \n  замінити всі дані в файлі db.json \n  на дані з файла db-start.json, \n  а потім запустити json-server за адресою: \n  http://localhost:3000");
+//! GET (отримати всіх користувачів):
