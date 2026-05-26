68d60004bf47c4d5ec9cf6c62fa28a32c10e7bf6
MyroslavShendrik <citrus.user2204@gmail.com>
2026-05-26 14:08:18 +0300

    Update practice-38.js

--- src/practice-38/js/practice-38.js
+++ src/practice-38/js/practice-38.js
@@ -6,3 +6,4 @@ console.log(
 //! Приклади CRUD HTTP-запитів з async/await та використанням json-server
 console.warn("Для демонстрації прикладів необхідно: \n  замінити всі дані в файлі db.json \n  на дані з файла db-start.json, \n  а потім запустити json-server за адресою: \n  http://localhost:3000");
 //! GET (отримати всіх користувачів):
+
