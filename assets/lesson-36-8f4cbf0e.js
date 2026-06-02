b6b9f97e4fee4825d5d1167e6e9f614cc41befb1
MyroslavShendrik <citrus.user2204@gmail.com>
2026-06-02 09:37:28 +0000

    Deploying to gh-pages from @ MyroslavShendrik/js-homework-vite@30ddc33f2869f6f84caef072a41e01617cfd90c5 🚀

---
+++ assets/lesson-36-8f4cbf0e.js
@@ -0,0 +1,10 @@
+import"./modulepreload-polyfill-3cfb730f.js";const i="https://jsonplaceholder.typicode.com/",o="posts",c=document.querySelector(".limit-input"),l=document.querySelector(".page-input"),s=document.querySelector(".fetch-btn"),r=document.querySelector(".posts-list");s.addEventListener("click",u);//! ================= MAIN FUNCTION =================
+function u(){const t=Number(c.value)||5,n=Number(l.value)||1;if(t<1||n<1){alert("Введіть коректні значення");return}a(n,t).then(p).catch(e=>{console.error(e),r.innerHTML="<li>Помилка завантаження</li>"})}//! ================= FETCH =================
+function a(t,n){return fetch(`${i}${o}?_page=${t}&_limit=${n}`).then(e=>{if(!e.ok)throw new Error("Network error");return e.json()})}//! ================= RENDER =================
+function p(t){if(!Array.isArray(t)||t.length===0){r.innerHTML="<li>Немає постів</li>";return}const n=t.map(e=>`
+        <li class="list-item">
+          <h3>${e.title}</h3>
+          <p>id: ${e.id}</p>
+          <p>${e.body}</p>
+        </li>
+      `).join("");r.innerHTML=n}
