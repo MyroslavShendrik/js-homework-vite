680123cd3691f098cae9527dddf022b688535cc1
MyroslavShendrik <citrus.user2204@gmail.com>
2026-07-03 14:47:17 +0300

    lesson

--- src/lesson-37-sw/js/homework-37.js
+++ src/lesson-37-sw/js/homework-37.js
@@ -1,1 +1,22 @@
-console.log("Star Wars")
+console.log("Star Wars");
+import Handlebars from "handlebars";
+import cardTemplate from "../templates/header.hbs?raw";
+const div = document.querySelector("#container")
+console.log("cardTemplate:",cardTemplate);
+const header = {
+  headerTitle: "Star Wars Fan Hub",
+  headerMenu: [
+    "Про сайт",
+    "Всесвіт",
+    "Сила",
+    "Джедаї і Сітхи",
+    "Війни",
+    "Енциклопедія",
+    "Магазин",
+  ],
+};
+
+const template = Handlebars.compile(cardTemplate);
+const markup = template(header);
+console.log("markup:",markup);
+div.innerHTML = markup
